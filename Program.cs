e772471511ce845490bc34048d16ce0f30bcecec
E. Keys <e.t.keys@gmail.com>
2019-03-24 15:18:00 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace randombg_dotnet {
+    public static class Program {
+        private static string _command = null;
+
+        public static void Main(string[] args) {
+            Setup(args);
+            switch (_command){
+                case "SET":
+                    throw new NotImplementedException();
+                    break;
+                case "UPDATEDB":
+                    UpdateDb.Run();
+                    break;
+            }
+        }
+
+        public static void Setup(string[] args){
+            if (args.Length < 1){
+                Console.WriteLine("Required arguments missing.");
+                PrintUsage();
+            }
+
+            string cmd = args[0].ToUpper();
+            switch (cmd){
+                case "SET":
+                case "UPDATEDB":
+                    _command = cmd;
+                    break;
+                default:
+                    Console.WriteLine("Unknown command {0}.", cmd);
+                    PrintUsage();
+                    break;
+            }
+        }
+
+        private static void PrintUsage(){
+            Environment.Exit(1);
+        }
+    }
+}
